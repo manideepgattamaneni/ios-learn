f38cba2c1da1855125223e177c527212d0328937
Manideep Gattamaneni <manideep.gattamaneni@teladochealth.com>
2023-12-12 11:02:06 -0500

    Added we split project

---
+++ WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,73 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Manideep Gattamaneni on 12/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var tipPercentage = 20
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    
+    @FocusState private var amountIsFocused:Bool
+    
+    var totalAmount: Double {
+        let tipSelection = Double(tipPercentage)
+
+        let tipValue = checkAmount / 100 * tipSelection
+        return checkAmount + tipValue
+    }
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let amountPerPerson = totalAmount / peopleCount
+
+        return amountPerPerson
+    }
+    
+    var body: some View {
+        NavigationStack{
+            Form{
+                Section{
+                    TextField("Enter Amount",value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD")).keyboardType(.decimalPad).focused($amountIsFocused)
+                    Picker("Enter Number of people",selection: $numberOfPeople){
+                        ForEach(2..<100){
+                            Text("\($0) people")
+                        }
+                    }.pickerStyle(.navigationLink)
+                }
+                Section("How much tip do you want to leave?"){
+                    Picker("Tip Percentage",selection: $tipPercentage){
+                        ForEach(0..<101){
+                            Text($0, format: .percent)
+                        }
+                    }.pickerStyle(.navigationLink)
+                }
+                Section("Total Amount"){
+                    Text(totalAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+                Section("Amount per person"){
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+            }
+            .navigationTitle("WeSplit")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar{
+                if(amountIsFocused){
+                    Button("Done"){
+                        amountIsFocused = false
+                    }
+                }
+            }
+        }
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Manideep Gattamaneni on 12/10/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
