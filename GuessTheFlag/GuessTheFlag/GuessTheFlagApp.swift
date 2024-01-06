92e6958b22f28d41f6add72106794c0411dd9549
Manideep Gattamaneni <manideep.gattamaneni@teladochealth.com>
2024-01-05 22:33:24 -0500

    Added a new project guess the flag

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Manideep Gattamaneni on 1/4/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
