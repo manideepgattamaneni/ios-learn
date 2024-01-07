134ba3461dd5932bb2d38a27bc6a3ac65914e03f
Manideep Gattamaneni <manideep.gattamaneni@teladochealth.com>
2024-01-07 15:59:34 -0500

    Added view modifiers projects with custom container example.

---
+++ ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Manideep Gattamaneni on 1/6/24.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
