297769974c355a277e30a6789e326269eabe3cef
Manideep Gattamaneni <manideep.gattamaneni@teladochealth.com>
2024-01-24 09:15:53 -0500

    Added word scramble project

---
+++ WordScrambler/WordScrambler/WordScramblerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScramblerApp.swift
+//  WordScrambler
+//
+//  Created by Manideep Gattamaneni on 1/24/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScramblerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
